ab15be89c464bc4245ee7cc38ea3002e54a49d15
Adarsh Singh <ss7456841@gmail.com>
2023-06-19 09:48:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tabs from './Tabs';
+
+const App = () => {
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: 'Content for Tab 1',
+    },
+    {
+      title: 'Tab 2',
+      content: 'Content for Tab 2',
+    },
+    {
+      title: 'Tab 3',
+      content: 'Content for Tab 3',
+    },
+  ];
+
+  return <Tabs tabs={tabs} />;
+};
+
+export default App;
